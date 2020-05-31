9011c070bfb8b11c2cfe6c6eff2d0b1ddc5fdb6c
Samuel Lin <Samuel3218888@gmail.com>
2020-05-31 15:19:18 -0700

    timer is now savable

--- javascript/timer.js
+++ javascript/timer.js
@@ -17,8 +17,6 @@ var diffTime;
 var days_completed;
 
 
-
-
 open.addEventListener('click', function(){
     if(formbutton.style.display == "block"){
         formbutton.style.display = "none"
@@ -29,6 +27,8 @@ open.addEventListener('click', function(){
 
 
 count_down_button.addEventListener('click', create_timer)
+
+/*
 
 // simple set up of today's date and the first day of the year
 const date = new Date();
@@ -42,13 +42,13 @@ const today_date = date.getDate();  // today's day
 const today = new Date(today_month + '/' + today_date + '/' + current_year)
 diffTime = Math.abs(today - first_Day);
 days_completed = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
-days_left = 365 - Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+days_left_Newyears = 365 - Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
 total_days = 365;
 
 
 //New years countdown
 strokeTransition(displays[0], days_completed , total_days)
-increaseNumber(displays[0], days_left , 'int');
+increaseNumber(displays[0], days_left_Newyears , 'int');
 
 console.log(displays[0])
 
@@ -57,11 +57,161 @@ console.log(displays[0])
 //Christmas countdown
 const christmas = new Date('12/25/2020')
 const total_days_christmas = Math.ceil(Math.abs(christmas - first_Day)/(1000 * 60 * 60 * 24)) ;
-days_left = total_days_christmas - Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+days_left_christmas = total_days_christmas - Math.ceil(diffTime / (1000 * 60 * 60 * 24))
 
 
 strokeTransition(displays[1], days_completed , total_days_christmas)
-increaseNumber(displays[1], days_left, 'int');
+increaseNumber(displays[1], days_left_christmas, 'int');
+*/
+
+load_timers()
+
+
+function load_timers(event){
+  const date = new Date();
+  const first_Day = new Date('1/1/' + date.getFullYear()) 
+  const New_years = new Date('12/31/' + date.getFullYear()) 
+  const Christmas = new Date('12/25/' + date.getFullYear()) 
+  var timer_name =['New years' , 'Christmas'] ;
+  var timer_day_created =[ first_Day , first_Day ];
+  var timer_end_date =[ New_years , Christmas];
+
+  if(localStorage.getItem('timer_name') === null){
+    console.log('timer_name created')
+    localStorage.setItem('timer_name', JSON.stringify(timer_name))
+  }else{
+    timer_name = JSON.parse(localStorage.getItem('timer_name'))
+    console.log(timer_name[0])
+  }
+
+  if(localStorage.getItem('timer_day_created') === null){
+    console.log('timer_day created')
+    localStorage.setItem('timer_day_created', JSON.stringify(timer_day_created ))
+  }else{
+    timer_day_created = JSON.parse(localStorage.getItem('timer_day_created'))
+    console.log(timer_day_created[1])
+  }
+
+  if(localStorage.getItem('timer_end_date') === null){
+    console.log('timer_end created')
+    localStorage.setItem('timer_end_date', JSON.stringify(timer_end_date ))
+  }else{
+    timer_end_date   = JSON.parse(localStorage.getItem('timer_end_date'))
+    console.log(timer_end_date[1])
+  }
+
+
+
+  for(let i = 0 ; i < timer_name.length ; i ++){
+      display_timer(timer_name[i] , timer_day_created[i] , timer_end_date[i])
+  }
+}
+
+
+
+function display_timer(event_name , timer_day_created, timer_end_date){
+
+    created_day = new Date(timer_day_created)
+    end_date = new Date(timer_end_date)
+
+
+    const date = new Date();
+    const current_year = date.getFullYear(); // today's year
+    const today_month = date.getMonth()+1;  // today's month
+    const today_date = date.getDate();  // today's day
+
+    const today = new Date(today_month + '/' + today_date + '/' + current_year)
+    var days_completed = parseInt((today- created_day) / (1000 * 60 * 60 * 24), 10); 
+    
+
+    
+
+    diffTime_daystillenddate = Math.abs(end_date - today)
+    daystillend_date = Math.ceil(diffTime_daystillenddate / (1000 * 60 * 60 * 24)); 
+
+
+
+
+    const diffTime_total_days = Math.abs(end_date - created_day);
+    const total_days = Math.ceil(diffTime_total_days / (1000 * 60 * 60 * 24)); 
+
+     //create list item
+     const note_display = document.createElement('li');
+     note_display.classList.add("note-display")
+ 
+ 
+     //create circle
+     const circle = document.createElement('div')
+     circle.classList.add("circle")
+ 
+ 
+     //create cricle svg
+     const circle_svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+ 
+     circle_svg.setAttribute( "width" , "200")
+     circle_svg.setAttribute("height" , "200")
+     circle_svg.classList.add("circle__svg")
+ 
+ 
+     //create circle path
+     const circle_path = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+     circle_path.classList.add("circle__progress")
+     circle_path.classList.add("circle__progress--path")
+ 
+     circle_path.setAttribute("cx" , "100")
+     circle_path.setAttribute("cy" , "100")
+     circle_path.setAttribute("r" , "75")
+    
+ 
+     //create cirle fill
+     const circle_fill= document.createElementNS("http://www.w3.org/2000/svg", "circle");
+     circle_fill.classList.add("circle__progress")
+     circle_fill.classList.add("circle__progress--fill")
+ 
+     circle_fill.setAttribute("cx" , "100")
+     circle_fill.setAttribute("cy" , "100")
+     circle_fill.setAttribute("r" , "75")
+ 
+ 
+     circle_svg.appendChild(circle_path)
+     circle_svg.appendChild(circle_fill)
+     circle.appendChild(circle_svg)
+ 
+ 
+     //create percent ( days inside the circle )
+     const percent = document.createElement('div')
+     percent.classList.add("percent")
+ 
+     
+     const percent_int = document.createElement('span')
+     percent_int.classList.add("percent__int")
+ 
+     const percent_dec = document.createElement('span')
+     percent_dec.classList.add("percent__dec")
+     percent_dec.innerText = 'days'
+ 
+ 
+     percent.appendChild(percent_int)
+     percent.appendChild(percent_dec)
+     circle.appendChild(percent)
+ 
+     //create label
+     const label = document.createElement('span')
+     label.classList.add('label')
+     label.innerText = event_name;
+ 
+     note_display.appendChild(circle)
+     note_display.appendChild(label)
+ 
+     display_container.append(note_display);
+
+     console.log('days completed:' + days_completed)
+     console.log('daystillend_date' + daystillend_date)
+ 
+     strokeTransition(note_display ,days_completed, total_days)
+     increaseNumber(note_display, daystillend_date, 'int');
+
+}
 
 
 
@@ -69,16 +219,48 @@ increaseNumber(displays[1], days_left, 'int');
 function create_timer(event){
 
     event.preventDefault()
+
+    var timer_name =[] ;
+    var timer_day_created =[];
+    var timer_end_date =[];
+
     
     var event_entered = event_name.value;
     var day_entered = day.value;
     var month_entered = month.value;
     var year_entered = year.value;
 
+    const date = new Date();
+    const current_year = date.getFullYear(); // today's year
+    const today_month = date.getMonth()+1;  // today's month
+    const today_date = date.getDate();  // today's day
+
+
+
     const entered = new Date(month_entered+ '/' + day_entered + '/' + year_entered );
     const current= new Date(today_month+ '/' + today_date+ '/' + current_year);
+
+      localStorage.getItem('timer_name' , JSON.stringify(timer_name))
+      timer_name = JSON.parse(localStorage.getItem('timer_name'))
+      timer_name.push(event_entered)
+      localStorage.setItem('timer_name' , JSON.stringify(timer_name))
+    
+
+      localStorage.getItem('timer_day_created' , JSON.stringify(timer_day_created))
+      timer_day_created = JSON.parse(localStorage.getItem('timer_day_created'))
+      timer_day_created.push(current)
+      localStorage.setItem('timer_day_created' , JSON.stringify(timer_day_created))
+  
+
+      localStorage.getItem('timer_end_date',  JSON.stringify(timer_end_date))
+      timer_end_date  = JSON.parse(localStorage.getItem('timer_end_date'))
+      timer_end_date.push(entered)    
+      localStorage.setItem('timer_end_date',  JSON.stringify(timer_end_date))
+  
     const diffTime = Math.abs(current - entered);
     const total_days = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+
+
 
     //create list item
     const note_display = document.createElement('li');
